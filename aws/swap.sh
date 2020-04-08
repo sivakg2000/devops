cb14f43cbd1479aec4a6b9f5af8e10cf7c13e39d
Siva Karuppiah <sivakg2000@gmail.com>
2020-04-08 13:21:33 +0530

    added swap script

---
+++ aws/swap.sh
@@ -0,0 +1,11 @@
+sudo swapon --show
+free -h
+df -h
+sudo fallocate -l 1G /swapfile
+ls -lh /swapfile
+sudo chmod 600 /swapfile
+ls -lh /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+free -h
