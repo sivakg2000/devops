f7c235dac52ffec2b81a85bf7c0da6ec420e3f61
Siva Karuppiah <sivakg2000@gmail.com>
2020-04-09 01:15:53 +0530

    adding vestcp setup with help of terraform.

--- aws/vestacp/main.tf
+++ aws/vestacp/main.tf
@@ -1,8 +1,26 @@
-resource "aws_instance" "vestacp" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+resource "aws_instance" "project-iac" {
+  ami                         = lookup(var.awsprops, "ami")
+  instance_type               = lookup(var.awsprops, "itype")
+  subnet_id                   = lookup(var.awsprops, "subnet")
+  associate_public_ip_address = lookup(var.awsprops, "publicip")
+  key_name                    = lookup(var.awsprops, "keyname")
 
-  tags = {
-    Name = "Vesta CP"
+
+  vpc_security_group_ids = [
+    aws_security_group.vestacp.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops                  = 150
+    volume_size           = 50
+    volume_type           = "gp2"
   }
+  tags = {
+    Name        = "Vesta CP"
+    Environment = "DEV"
+  }
+
+  depends_on = [aws_security_group.vestacp]
 }
+
+
---
+++ aws/vestacp/network.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "vestacp" {
+  name        = lookup(var.awsprops, "secgroupname")
+  description = lookup(var.awsprops, "secgroupname")
+  vpc_id      = lookup(var.awsprops, "vpc")
+
+  // To Allow SSH Transport
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 (HTTP) Transport
+  ingress {
+    from_port   = 80
+    protocol    = ""
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 443 (HTTPS) Transport
+  ingress {
+    from_port   = 443
+    protocol    = ""
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 8083 (VESTCP)Transport
+  ingress {
+    from_port   = 8083
+    protocol    = ""
+    to_port     = 8083
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 25 (SMTP) Transport
+  ingress {
+    from_port   = 25
+    protocol    = ""
+    to_port     = 25
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
