688652364f7e5bd525a93360630997584cdcc07e
Siva Karuppiah <sivakg2000@gmail.com>
2020-04-10 00:58:42 +0530

    added vesta install instructions into userdata script

---
+++ aws/vestacp/userdata.sh
@@ -0,0 +1,4 @@
+
+sudo su
+curl -O http://vestacp.com/pub/vst-install.sh
+bash vst-install.sh --nginx yes --apache yes --phpfpm no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim yes --dovecot yes --spamassassin yes --clamav yes --softaculous yes --mysql yes --postgresql no --hostname test-host --email test@test.com --password Test@123
